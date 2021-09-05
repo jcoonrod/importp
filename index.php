16a0771984e656b77d6a7a9ef5561305eed01fb2
John Coonrod <john.coonrod@thp.org>
2021-09-04 20:26:45 -0400

    fixes

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-$xml = file_get_contents('../importdp/jc.xml');
-$p = xml_parser_create();
+<?php
+$xml = file_get_contents('../dpimport/jc.xml');
+$p = xml_parser_create('');
 xml_parse_into_struct($p, $simple, $vals, $index);
 xml_parser_free($p);
 echo "Index array\n";
